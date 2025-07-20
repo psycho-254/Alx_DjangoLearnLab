ea28401cf399e40c038b67394f13481d2677c976
psycho-254 <abdul.code254@gmail.com>
2025-07-20 22:55:16 +0300

    creating Django Views and URL Configuration

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from django.views.generic import DetailView
+from django.views.generic.detail import DetailView
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
 from django.core.exceptions import PermissionDenied
