3cf956d761e2cae89b9ecc15630212517d1e321a
psycho-254 <abdul.code254@gmail.com>
2025-07-20 18:22:47 +0300

    configuring and customizing the admin interface

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Book
 
 # Register your models here.
+admin.site.register(Book)
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'publication_year')
+    search_fileds = ('titles', 'author')
+
+admin.site.register(Book, BookAdmin)
